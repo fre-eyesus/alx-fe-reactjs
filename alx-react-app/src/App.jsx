d824cb84ff4fe704e898140127751ea5f0933803
Fireweld Tesfaye <freyesus12@gmail.com>
2025-02-16 22:29:28 +0300

    implement props by creating userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Footer'
 import MainContent from './components/MainContent'
 import Footer from './components/Header'
+import UserProfile from './components/UserProfile'
 import './App.css'
 
 
@@ -33,6 +34,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
       <WelcomeMessage />
       <Header />
       <Footer />
