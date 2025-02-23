1a3c31631d4600d58bf2ebb7f0746b41bea2a1f1
Fireweld Tesfaye <freyesus12@gmail.com>
2025-02-23 23:51:06 +0300

    create react app

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return (
+      <footer style={{ textAlign: "center", padding: "10px", background: "#333", color: "white" }}>
+        <p>&copy; 2025 My Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
