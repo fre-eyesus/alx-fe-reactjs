9ce1eff2351c412dc0cf7c13c6de1c034eaf306d
Fireweld Tesfaye <freyesus12@gmail.com>
2025-02-23 11:40:24 +0300

    make the existing react app to use prop drilling

---
+++ alx-react-app-new-props/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+function MainContent() {
+  return (
+      <main style={{ padding: '20px', backgroundColor: '#f4f4f4', textAlign: 'center' }}>
+          <h2 style={{ color: 'darkblue' }}>Welcome to the World of Cities</h2>
+          <p style={{ fontSize: '18px', color: 'black' }}>
+              I love to visit New York, Paris, and Tokyo.
+          </p>
+      </main>
+  );
+}
+
+export default MainContent;
