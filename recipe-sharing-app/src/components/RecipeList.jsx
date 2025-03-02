951666982d4fd24846e77c37f0414c1ab6e3f922
Fireweld Tesfaye <freyesus12@gmail.com>
2025-03-02 15:50:08 +0300

    Add Link import to RecipeList component for navigation

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useRecipeStore from '../recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
