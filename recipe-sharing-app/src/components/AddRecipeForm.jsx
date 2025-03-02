954f50b630b2ca106276f3f012a15c2de2bbd6ff
Fireweld Tesfaye <freyesus12@gmail.com>
2025-03-02 16:23:09 +0300

    Add comment to AddRecipeForm component

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 import { useRecipeStore } from './recipeStore';
-
+// comment
 const AddRecipeForm = () => {
   const addRecipe = useRecipeStore(state => state.addRecipe);
   const [title, setTitle] = useState('');
