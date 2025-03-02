f6ca469672de48d18daca3022e29007156d8a4c9
Fireweld Tesfaye <freyesus12@gmail.com>
2025-03-02 16:29:05 +0300

    update recipeStore.js to include

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,28 +1,3 @@
-// import create from 'zustand';
-
-// const useRecipeStore = create(set => ({
-//   recipes: [], 
-//   favorites: [], 
-  
-  
-//   toggleFavorite: (recipeId) => set(state => ({
-//     favorites: state.favorites.includes(recipeId)
-//       ? state.favorites.filter(id => id !== recipeId) 
-//       : [...state.favorites, recipeId] 
-//   })),
-
-//   recommendations: [], 
-  
-
-//   generateRecommendations: () => set(state => {
-//     const recommended = state.recipes.filter(recipe =>
-//       state.favorites.includes(recipe.id) && Math.random() > 0.5
-//     );
-//     return { recommendations: recommended };
-//   }),
-// }));
-
-// export default useRecipeStore;
 import create from 'zustand';
 
 const useRecipeStore = create(set => ({
