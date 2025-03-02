453c2525d11377f6d07a575a4da35dcd5de35866
Fireweld Tesfaye <freyesus12@gmail.com>
2025-03-02 14:29:54 +0300

    change the import indentation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import React from 'react';
-import RecipeList  from './components/RecipeList';
+import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import './App.css'
 
