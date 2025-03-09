f936c4d05c7c6c55496591fcd58991a7db52a70a
Fireweld Tesfaye <freyesus12@gmail.com>
2025-03-09 23:47:55 +0300

    add useAuth

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth"; 
 
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth(); 
 
-const ProtectedRoute = ({ isAuthenticated, children }) => {
   
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
