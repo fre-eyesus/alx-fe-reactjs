d896a8b15499e687b8e243c7ffbdb5ef293811d5
Fireweld Tesfaye <freyesus12@gmail.com>
2025-03-09 23:41:37 +0300

    refactor: update import paths and improve dynamic route naming in App component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
-import BlogPost from "./components/BlogPost";
-import Login from "./components/Login";
-import NotFound from "./components/NotFound";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import BlogPost from "./pages/BlogPost";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
@@ -18,8 +18,8 @@ function App() {
         {/* Protected Route */}
         <Route path="/profile/*" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Profile /></ProtectedRoute>} />
 
-        {/* Dynamic Route Example */}
-        <Route path="/blog/:postId" element={<BlogPost />} />
+        {/* Dynamic Route for Blog Post */}
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* Ensure dynamic route is here */}
 
         {/* Login Route */}
         <Route path="/login" element={<Login setAuth={setIsAuthenticated} />} />
