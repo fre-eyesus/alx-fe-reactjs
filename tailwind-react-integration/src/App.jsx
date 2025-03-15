d4d0192983ac077ab945fafb6e1ee4ffcaf6e822
Fireweld Tesfaye <freyesus12@gmail.com>
2025-03-16 00:19:02 +0300

    configure tailwindcss on react app

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="text-center text-gray-700">Hey this is tailwind</div>
+    </>
+  )
+}
+
+export default App
