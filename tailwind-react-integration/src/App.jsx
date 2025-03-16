49e5d3bb0d4dda718042695d4a9934896a1d2711
Fireweld Tesfaye <freyesus12@gmail.com>
2025-03-16 10:08:59 +0300

    create and style userprofile component

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import UserProfile from './components/userProfile'
 
 function App() {
   return (
     <>
+    <UserProfile />
       <div className="text-center text-gray-700">Hey this is tailwind</div>
     </>
   )
