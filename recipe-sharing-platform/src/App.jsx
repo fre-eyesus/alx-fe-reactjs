697207c98686e28cdd7c58e56b87542993310286
Fireweld Tesfaye <freyesus12@gmail.com>
2025-03-19 22:31:48 +0300

    prepare mock up data in data.json to display in the component

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
  
-      <h1 className="text-center">Recipe Sharing Platform</h1>
+      <h1 className="text-center text-red-700">Recipe Sharing Platform</h1>
+      <p className="text-center text-red-700">Welcome to the Recipe Sharing Platform!</p>
       
     </>
   )
