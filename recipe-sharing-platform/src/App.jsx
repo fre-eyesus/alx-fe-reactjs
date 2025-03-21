9756e0ee2cdef1cea2de86d4e61ac53d21d0300b
Fireweld Tesfaye <freyesus12@gmail.com>
2025-03-22 02:21:30 +0300

    add routing and recipe detail view; integrate react-router-dom

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,23 @@
 import './index.css'
 import HomePage from './components/HomePage'
+import RecipeDetail from './components/RecipeDetail'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <HomePage />
+  <Router>
+  
+      <Routes>  
+  
+         <Route path="/" element={<HomePage />} />
+         <Route path="/recipe/:id" element ={<RecipeDetail/>} />
+  
+      </Routes>
+  </Router>
+
+
     </>
   )
 }
---
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -0,0 +1,52 @@
+import {useEffect, useState} from "react"
+
+function RecipeDetail(){
+    const [recipe, setRecipe] = useState([nul])
+    const  [loading, setLoading] = useState(true)
+    const {id} = useParams()
+    useEffect(() =>{
+        try{
+            const fetchRecipe = async () =>{
+            const response = await fetch("/data.json");
+            const data = await response.json();
+            const selectedRecipe = data.find((r) => r.id === parseInt(id));
+        }
+        
+        if (selectedRecipe) {
+            setRecipe(selectedRecipe);
+          } else {
+            console.error("Recipe not found");
+          }
+
+    }
+    catch(error){
+        console.log("failed to fetch recipe", error)
+    }
+    finally{
+        setLoading(false)
+    }
+
+    fetchRecipe();
+    },[id])
+
+    if(loading){
+        return <div className="text-center text-gray-500">Loading...</div>;
+    }
+
+    if(!recipe){
+        return <div className="text-center text-gray-500">404 Recipe not found</div>
+    }
+
+    return(
+        <>
+        <div className="max-w-2xl mx-auto p-6 bg-white shadow-lg rounded-lg">
+            <h1 className="text-3xl font-bold text-center mb-4">{recipe.title}</h1>
+            <img src={recipe.image} alt={recipe.title} className="w-full h-64 object-cover rounded-md" />
+            <p className="mt-4 text-gray-700">{recipe.summary}</p>
+        </div>        
+        </>
+    )
+
+}
+
+export default RecipeDetail 
