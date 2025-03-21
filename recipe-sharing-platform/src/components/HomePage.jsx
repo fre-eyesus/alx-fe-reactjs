66300131b29c04b683cb11fdc3e18ba9e33d12eb
Fireweld Tesfaye <freyesus12@gmail.com>
2025-03-22 02:37:46 +0300

    import react-router-dom library to HomePage.jsx

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function HomePage(){
     const [recipes, setRecipes] = useState([])
     useEffect(() => {
